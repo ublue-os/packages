efe31d72f29fd26349b3070c765bb81563288e58
RJ Trujillo <eyecantcu@pm.me>
2024-04-28 12:36:17 -0600

    fix(bluefin-cli): Swap su-exec for sudo in firstrun scripts
    Signed-off-by: RJ Trujillo <eyecantcu@pm.me>

--- wolfi/bluefin-base/usr/share/fish/vendor_conf.d/00-bluefin-cli-brew-firstrun.fish
+++ wolfi/bluefin-base/usr/share/fish/vendor_conf.d/00-bluefin-cli-brew-firstrun.fish
@@ -5,7 +5,7 @@ if test "$(id -u)" -gt "0"
   if test ! -f /etc/linuxbrew.firstrun
     printf "\nBluefin-CLI First Run Setup\n\n"
     printf "Setting up sudo for %s%s%s...\t\t\t " "$bold" "$USER" "$normal"
-    echo "#$UID ALL = (root) NOPASSWD:ALL" | su-exec root tee -a /etc/sudoers > /dev/null
+    echo "#$UID ALL = (root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
     printf "%s[ OK ]%s\n" "$blue" "$normal"
   end
 
@@ -19,27 +19,27 @@ if test "$(id -u)" -gt "0"
       mkdir -p "$linuxbrew_home"
     end
     if test ! -d /home/linuxbrew
-      su-exec root mkdir -p /home/linuxbrew
+      sudo mkdir -p /home/linuxbrew
     end
-    su-exec root mount --bind "$linuxbrew_home" /home/linuxbrew
-    su-exec root cp -R /home/homebrew/.linuxbrew /home/linuxbrew/
-    su-exec root chown -R "$UID" /home/linuxbrew
+    sudo mount --bind "$linuxbrew_home" /home/linuxbrew
+    sudo cp -R /home/homebrew/.linuxbrew /home/linuxbrew/
+    sudo chown -R "$UID" /home/linuxbrew
     set -e linuxbrew_home
     printf "%s[ OK ]%s\n" "$blue" "$normal"
   end
 
   if test ! -d /usr/local/share/bash-completion/completions
     printf "Setting up Tab-Completions...\t\t\t "
-    su-exec root mkdir -p /usr/local/share/bash-completion
-    su-exec root mount --bind /run/host/usr/share/bash-completion /usr/local/share/bash-completion
+    sudo mkdir -p /usr/local/share/bash-completion
+    sudo mount --bind /run/host/usr/share/bash-completion /usr/local/share/bash-completion
     if test -x /run/host/usr/bin/ujust
-      su-exec root ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/ujust
+      sudo ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/ujust
     end
     printf "%s[ OK ]%s\n" "$blue" "$normal"
   end
 
   if test ! -f /etc/linuxbrew.firstrun
-    su-exec root touch /etc/linuxbrew.firstrun
+    sudo touch /etc/linuxbrew.firstrun
     printf "\nBluefin-CLI first run complete!\n\n"
   end
 end
