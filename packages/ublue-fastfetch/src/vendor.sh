40b7e3a01ac2347582df15539827f7dfe63b8ff9
OhNoOhNOOO <107513274+OhNoOhNOOO@users.noreply.github.com>
2025-05-03 15:24:40 -0500

    ublue-fastfetch: remove alias for hyfetch (#407)

--- packages/ublue-fastfetch/src/vendor.sh
+++ packages/ublue-fastfetch/src/vendor.sh
@@ -1,4 +1,3 @@
 alias neofetch='/usr/libexec/ublue-fastfetch'
 alias neowofetch='/usr/libexec/ublue-fastfetch'
-alias hyfetch='/usr/libexec/ublue-fastfetch'
 alias fastfetch='/usr/libexec/ublue-fastfetch'
