c8bbc8935bee950cddb4425c7754b6283a3cc5d1
Jon Mulder <jon.e.mulder@gmail.com>
2025-05-12 22:12:10 -0400

    fix(ublue-setup-services): fix secure boot sb-state check
    Update `check-sb-key.sh` to have `mokutil --sb-state` not equal 0 vs 1.
    
    Fixes: https://github.com/ublue-os/packages/issues/457

--- packages/ublue-setup-services/src/scripts/check-sb-key.sh
+++ packages/ublue-setup-services/src/scripts/check-sb-key.sh
@@ -6,17 +6,17 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 get_config() {
-    SETUP_CONFIG_FILE="${SETUP_CONFIG_FILE:-/etc/ublue-os/setup.json}"
-    QUERY="$1"
-    FALLBACK="$2"
-    shift
-    shift
-    OUTPUT="$(jq -r "$QUERY" "$SETUP_CONFIG_FILE" 2>/dev/null || echo "$FALLBACK")"
-    if [ "$OUTPUT" == "null" ] ; then
-        echo "$FALLBACK"
-        return
-    fi
-    echo "$OUTPUT"
+  SETUP_CONFIG_FILE="${SETUP_CONFIG_FILE:-/etc/ublue-os/setup.json}"
+  QUERY="$1"
+  FALLBACK="$2"
+  shift
+  shift
+  OUTPUT="$(jq -r "$QUERY" "$SETUP_CONFIG_FILE" 2>/dev/null || echo "$FALLBACK")"
+  if [ "$OUTPUT" == "null" ]; then
+    echo "$FALLBACK"
+    return
+  fi
+  echo "$OUTPUT"
 }
 
 WARNING_MSG="This machine has secure boot turned on, but you haven't enrolled Universal Blue's keys. Failing to enroll these before rebooting **may cause your system to fail to boot**. Follow [the documentation](https://docs.projectbluefin.io/installation#secure-boot) ~for key enrollment information."
@@ -27,18 +27,18 @@ IS_THIS_ENABLED="$(get_config '."check-secureboot"' "true")"
 mokutil --sb-state | grep -q enabled
 SB_ENABLED=$?
 
-if [ $SB_ENABLED -ne 1 ]; then
-    echo "Secure Boot disabled. Skipping..."
-    exit 0
+if [ $SB_ENABLED -ne 0 ]; then
+  echo "Secure Boot disabled. Skipping..."
+  exit 0
 fi
 
-if [ "$IS_THIS_ENABLED" == "false" ] ; then
-    exit 0
+if [ "$IS_THIS_ENABLED" == "false" ]; then
+  exit 0
 fi
 
-if mokutil --test-key "$KEY_DER_FILE"; then 
-    echo "$WARNING_MSG" > $KEY_WARN_FILE
+if mokutil --test-key "$KEY_DER_FILE"; then
+  echo "$WARNING_MSG" >$KEY_WARN_FILE
 else
-    [ -e $KEY_WARN_FILE ] && rm $KEY_WARN_FILE
+  [ -e $KEY_WARN_FILE ] && rm $KEY_WARN_FILE
 fi
 exit 0
